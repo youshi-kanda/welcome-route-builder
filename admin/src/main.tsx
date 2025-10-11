264f6ad297cee1261771090f1873b711791b7c0e
youshi-kanda <kanda02.1203@gmail.com>
2025-10-12 03:22:04 +0900

    fix: main.tsxのインポートパスから.tsx拡張子を削除

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 const queryClient = new QueryClient({
   defaultOptions: {
